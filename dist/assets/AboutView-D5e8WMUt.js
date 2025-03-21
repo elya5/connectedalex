2d6c28e98e8d06da1e62cb357cbe1ba50c474a42
Elias Entrup <elias.entrup@tib.eu>
2025-03-21 12:43:51 +0100

    Undo commit

--- dist/assets/AboutView-D5e8WMUt.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,a as s,o as a}from"./index-Dc_XuC5-.js";import"./pdfjs-B-jxax5e.js";const n={},r={class:"about"};function c(i,e){return a(),t("div",r,e[0]||(e[0]=[s("h1",null,"This is an about page",-1)]))}const u=o(n,[["render",c]]);export{u as default};
